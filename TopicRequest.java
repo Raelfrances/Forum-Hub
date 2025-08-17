f27c5158570883fb39ba21fb513dfc79ca1963ff
Israel Alexandre Teófilo Santos <157400417+Raelfrances@users.noreply.github.com>
2025-08-17 20:30:48 -0300

    Create TopicRequest.java

---
+++ TopicRequest.java
@@ -0,0 +1,10 @@
+public class TopicRequest {
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String message;
+    @NotBlank
+    private String course;
+    @NotBlank
+    private String author;
+}
